40720388178f3378f182c238330ea2c8584de2ed
George Andrinopoulos <george.andrinopoulos@leanix.net>
2023-04-09 08:58:39 +0300

    CID-1502 Add logging statements

--- src/main/kotlin/net/leanix/vsm/sbomBooster/VsmSbomBoosterApplication.kt
+++ src/main/kotlin/net/leanix/vsm/sbomBooster/VsmSbomBoosterApplication.kt
@@ -3,12 +3,14 @@ package net.leanix.vsm.sbomBooster
 import net.leanix.vsm.sbomBooster.configuration.PropertiesConfiguration
 import net.leanix.vsm.sbomBooster.service.GitHubApiService
 import net.leanix.vsm.sbomBooster.service.ProcessService
+import net.leanix.vsm.sbomBooster.service.SummaryReportService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicInteger
 
 @SpringBootApplication
@@ -16,7 +18,8 @@ import java.util.concurrent.atomic.AtomicInteger
 class VsmSbomBoosterApplication(
     private val gitHubApiService: GitHubApiService,
     private val processService: ProcessService,
-    private val propertiesConfiguration: PropertiesConfiguration
+    private val propertiesConfiguration: PropertiesConfiguration,
+    private val summaryReportService: SummaryReportService
 ) : CommandLineRunner {
     companion object {
         private val logger: Logger = LoggerFactory.getLogger(VsmSbomBoosterApplication::class.java)
@@ -25,6 +28,23 @@ class VsmSbomBoosterApplication(
 
     override fun run(vararg args: String?) {
         logger.info("Starting SBOM extraction application.")
+
+        summaryReportService.appendRecord("Started VSM SBOM Booster at: ${LocalDateTime.now()}\n")
+        summaryReportService.appendRecord(
+            "VSM SBOM Booster ran with the following parameters " +
+                "(secrets are omitted): \n"
+        )
+        summaryReportService.appendRecord("MOUNTED_VOLUME: ${propertiesConfiguration.mountedVolume}\n")
+        summaryReportService.appendRecord(
+            "GITHUB_GRAPHQL_API_URL: " +
+                "${propertiesConfiguration.githubGraphqlApiUrl}\n"
+        )
+        summaryReportService.appendRecord("GITHUB_ORGANIZATION: ${propertiesConfiguration.githubOrganization}\n")
+        summaryReportService.appendRecord("REGION: ${propertiesConfiguration.region}\n")
+        summaryReportService.appendRecord("SOURCE_TYPE: ${propertiesConfiguration.sourceType}\n")
+        summaryReportService.appendRecord("SOURCE_INSTANCE: ${propertiesConfiguration.sourceInstance}\n")
+        summaryReportService.appendRecord("CONCURRENCY_FACTOR: ${propertiesConfiguration.concurrencyFactor}\n\n")
+
         val username = gitHubApiService.getUsername(propertiesConfiguration.githubToken)
         val repositories = gitHubApiService.getRepositories(
             propertiesConfiguration.githubToken, propertiesConfiguration.githubOrganization
@@ -40,6 +60,8 @@ class VsmSbomBoosterApplication(
                 propertiesConfiguration.region
             )
         }
+
+        summaryReportService.appendRecord("\nFinished VSM SBOM Booster at ${LocalDateTime.now()} \n")
     }
 }
 
